4fc212d1678f8b641845c5c5ac11a2b7c5e3d35b
ham8821 <ham38538821@gmail.com>
2019-09-10 14:40:25 +1200

    Update app.js
    error handling added

--- app.js
+++ app.js
@@ -10,4 +10,20 @@ app.use(morgan('dev'));
 // Routes which should handle request
 app.use('/products', productRoutes );
 app.use('/orders', OrderRoutes );
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status=404;
+    next(error);
+});
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
 module.exports= app;
