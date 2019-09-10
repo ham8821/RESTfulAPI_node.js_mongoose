16cbbe626c1a349b43249d6e8ad5552fec9a4ba1
ham8821 <ham38538821@gmail.com>
2019-09-10 14:15:58 +1200

    node morgan added
    logging in

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express= require('express');
 const app= express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const OrderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 // Routes which should handle request
-app.use('/products', productRoutes )
-app.use('/orders', OrderRoutes )
+app.use('/products', productRoutes );
+app.use('/orders', OrderRoutes );
 module.exports= app;
